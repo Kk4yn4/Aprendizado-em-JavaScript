c0ed7c4ef8877939cb00f1c8ff076ef7511e0cd4
Kk4yn4 <133814829+Kk4yn4@users.noreply.github.com>
2023-06-28 01:04:20 -0300

    Site idade...

--- aulas/script.js
+++ aulas/script.js
@@ -3,18 +3,21 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora = data.getHours()
+var hora =23//data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
 
 if(hora >= 0 && hora < 12) {
     //BOM DIA!
     img.src = '/Aprendizado-em-JavaScript/imagens/MANHA2.0.png'
+    document.body.style.background = '#99abaf'
 } else if (hora >= 12 && hora < 18) {
     //BOA TARDE!
     img.src = '/Aprendizado-em-JavaScript/imagens/TARDE2.0.png'
+    document.body.style.backgroundColor = '#985c3e'
 }else{
     //BOA NOITE!
     img.src = '/Aprendizado-em-JavaScript/imagens/NOITE2.0.png'
+    document.body.style.background = '#221a0a'
 }
 
 }
