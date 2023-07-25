29f97c0ed75cb3c09bddb5c70d6145e70bf72e7d
Kk4yn4 <133814829+Kk4yn4@users.noreply.github.com>
2023-07-24 23:02:02 -0300

    aula 15 Arrays

---
+++ aulas/aula15.js
@@ -0,0 +1,18 @@
+/*var valores = [7,8,5,2,0,4,5,6,5]
+for(pos = 0; pos < valores.length; pos++){
+    console.log(`${pos} tem o valor ${valores[pos]} `)
+for(var pos in valores){
+    console.log(`${pos} o valor e ${valores[pos]}`)*/
+
+    var num = [0,5,9,8,7,4,1]
+    num.sort()
+    console.log(`${num}`)
+   
+    let pos = num.indexOf(4)
+    
+
+    if(pos == -1){
+        console.log('[ERRO]')
+    }else{
+        console.log(`O numero  e ${pos}`)
+    }
